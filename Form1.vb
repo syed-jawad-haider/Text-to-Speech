2d8d6ef64abcd34ce0df04f3a5345174e9db99e6
syed-jawad-haider <l217509@lhr.nu.edu.pk>
2025-03-23 16:36:21 +0500

    Initial commit

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Speech.Synthesis
+Public Class Form1
+    Dim AI As SpeechSynthesizer = New SpeechSynthesizer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AI.SelectVoiceByHints(VoiceGender.Male)
+
+        AI.SpeakAsync(TextBox1.Text)
+
+    End Sub
+End Class
